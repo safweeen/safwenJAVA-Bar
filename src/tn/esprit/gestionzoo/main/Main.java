fc6baaa95b202732a2e3f43889729d93fec77b65
safweeen <123266920+safweeen@users.noreply.github.com>
2024-12-17 16:04:46 +0100

    prosit8

--- src/tn/esprit/gestionzoo/main/Main.java
+++ src/tn/esprit/gestionzoo/main/Main.java
@@ -1,73 +1,142 @@
 package tn.esprit.gestionzoo.main;
 import tn.esprit.gestionzoo.entities.*;
-import tn.esprit.gestionzoo.entities.Zoo;
-
+import tn.esprit.gestionzoo.exceptions.*;
 
 public class Main {
     public static void main(String[] args) {
 
-     /*   Animal lion = new Animal("safwen", "Lion", 6, true);
-        Animal tiger = new Animal("smile", "Tigre", 5, true);
-        Animal elephant = new Animal("khalil", "Éléphant", 10, false);
-        Animal lion2 = new Animal("Safwen", "Lion", 6, true);
+        /*   // Création de l'objet tn.esprit.gestionzoo.entities.Animal (lion)
+        Animal lion = new Animal("Mufasa", "Lion", 6, true);
+        Animal tiger = new Animal("Shere Khan", "Tigre", 5, true);
+        Animal elephant = new Animal("Baloo", "Éléphant", 10, false);
+        Animal lion2 = new Animal("Mufasa", "Lion", 6, true);
+
+        Zoo myZoo2 = new Zoo("Havana", "Zaghouan");
 
 
-        Zoo myZoo = new Zoo("amazon", "Tunis", 25);
-        Zoo myZoo2 = new Zoo("echkel", "bizerte", 19);
-
-
-
+        // Ajout de l'animal au zoo
         myZoo.addAnimal(lion);
         myZoo.addAnimal(tiger);
         myZoo.addAnimal(elephant);
         myZoo.addAnimal(lion2);
 
+
+
+        // Affichage des détails du zoo et des animaux
         myZoo.displayZoo();
         lion.displayAnimal();
 
         myZoo.displayZoo();
 
-
-        int index = myZoo.searchAnimal("safwen");
+        // Recherche et suppression d'un animal
+        int index = myZoo.searchAnimal("Mufasa");
         if (index != -1) {
             System.out.println("tn.esprit.gestionzoo.entities.Animal trouvé à l'index: " + index);
         }
         myZoo.removeAnimal(tiger);
 
-
+        // Comparaison entre les zoos
         Zoo biggerZoo = Zoo.comparerZoo(myZoo, myZoo2);
         if (biggerZoo != null) {
             System.out.println("Le zoo avec le plus d'animaux est: " + biggerZoo.getName());
         }
-        System.out.println(myZoo);*/
+
+
+        System.out.println(myZoo);                // Appel implicite à toString()
+    */
 
 
 
-                            Animal animal = new Animal("Mammifère", "Lion", 5, true);
-                            Aquatic aquatic = new Aquatic("Poisson", "Requin", 8, false, "Océan");
-                            Terrestrial terrestrial = new Terrestrial("Mammifère", "Éléphant", 10, true, 4);
-                            Dolphin dolphin = new Dolphin("Mammifère", "Dauphin", 6, true, "Mer", 25.5f);
-                            Penguin penguin = new Penguin("Oiseau", "Pingouin", 3, false, "Antarctique", 100.0f);
+        /*Aquatic aquatic = new Aquatic("Poisson", "Thon", 2, false, "Océan");
+        Terrestrial terrestrial = new Terrestrial("Reptile", "Tortue", 100, false, 4);
+        Dolphin dolphin1 = new Dolphin("Cétacé", "Dauphin Bleu", 8, true, "Mer Méditerranée", 40.5f);
+        Dolphin dolphin2 = new Dolphin("Cétacé", "Dauphin Gris", 5, true, "Océan Atlantique", 30.2f);
+        Penguin penguin1 = new Penguin("Oiseau", "Pingouin Empereur", 3, true, "Antarctique", 15.3f);
+        Penguin penguin2 = new Penguin("Oiseau", "Pingouin de Humboldt", 2, true, "Amérique du Sud", 10.0f);
 
 
-                            System.out.println(animal);
-                            System.out.println(aquatic);
-                            System.out.println(terrestrial);
-                            System.out.println(dolphin);
-                            System.out.println(penguin);
+        System.out.println(aquatic);
+        System.out.println(terrestrial);
+        System.out.println(dolphin1);
+        System.out.println(dolphin2);
+        System.out.println(penguin1);
+        System.out.println(penguin2);
 
-                            aquatic.swim();
-                            dolphin.swim();
-                            penguin.swim();
-                    }
-            }
+        myZoo.addAquaticAnimal(dolphin1);
+        myZoo.addAquaticAnimal(dolphin2);
+        myZoo.addAquaticAnimal(penguin1);
+        myZoo.addAquaticAnimal(penguin2);
 
+        myZoo.displayAllAquaticSwimming();
+        System.out.println("Liste des animaux aquatiques et leur nage :");
 
-
-
-
-
-
-
+        myZoo.displayNumberOfAquaticsByType();
+        System.out.println("\nle nombre des animaux aquatiques :");
 
 
+        float maxDepth = myZoo.maxPenguinSwimmingDepth();
+        System.out.println("La profondeur maximale des pingouins dans le zoo est : " + maxDepth + " mètres.");*/
+
+
+       /* dolphin1.swim();
+        dolphin2.swim();
+        penguin1.swim();
+        penguin2.swim();*/
+
+       /* Dolphin dolphin3 = new Dolphin("Cétacé", "Dauphin Bleu", 8, true, "Mer Méditerranée", 40.5f);
+        boolean dolphinEqual = dolphin1.equals(dolphin3);
+        System.out.println("Les deux dophins sont-ils identiques ?: " + dolphinEqual);*/
+
+
+       /* try {
+            // Ajout d'animaux avec gestion des exceptions
+            Animal lion = new Animal("Mufasa", "Lion", 6, true);
+            myZoo.addAnimal(lion);
+            System.out.println("Nombre d'animaux dans le zoo : " + myZoo.getNbrAnimals());
+
+            Animal tiger = new Animal("Félin", "Tigre", -1, true);  // Âge négatif pour tester InvalidAgeException
+            myZoo.addAnimal(tiger);  // Cela lèvera une InvalidAgeException
+
+            Animal elephant = new Animal("Mammifère", "Éléphant", 10, false);
+            myZoo.addAnimal(elephant);
+            System.out.println("Nombre d'animaux dans le zoo : " + myZoo.getNbrAnimals());
+
+            Animal zebra = new Animal("Équidé", "Zèbre", 5, false);
+            myZoo.addAnimal(zebra);  // Cela lèvera une ZooFullException
+
+        } catch (ZooFullException e) {
+            System.out.println(e.getMessage());
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }*/
+
+        Zoo myZoo = new Zoo("Belvidaire", "Tunis");
+
+        try {
+            // Création des objets
+            Aquatic shark = new Aquatic("Poisson", "Requin", 5, false, "Océan") {
+                @Override
+                public void swim() {
+                    System.out.println(getName() + " nage rapidement dans l'eau.");
+                }
+            };
+            Penguin penguin = new Penguin("Oiseau", "Pingouin Empereur", 3, true, "Antarctique", 15.3f);
+
+            Terrestrial bear = new Terrestrial("Mammifère", "Ours", 7, true, 4);
+
+            // Tests des méthodes alimentaires
+            shark.eatMeat(Food.MEAT);   // Le requin devrait manger de la viande
+            bear.eatMeat(Food.MEAT);    // L'ours peut manger de la viande
+            bear.eatPlant(Food.PLANT);  // L'ours peut manger des plantes
+            bear.eatPlantAndMeat(Food.BOTH);  // L'ours peut manger les deux
+            penguin.eatMeat(Food.MEAT); // Le pingouin mange de la viande car il est carnivore
+
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+
+}
